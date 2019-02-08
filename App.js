c50544d5015d7b235b1ac85b3f6e204b61bda1d7
highbury1999 <henryliaoxuyang@gmail.com>
2019-02-08 14:47:33 +1100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, Image} from 'react-native';
+import Galleries from './Components/Galleries';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
+import  Profile  from './Components/Profile';
+import  Cart  from './Components/Cart';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+}
+
+class ArtWorks extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: { screen: HomeScreen },
+    ArtWorks: { screen: ArtWorks },
+    Galleries:{screen:Galleries},
+    Cart: {screen:Cart},
+    Profile: {screen: Profile}
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          return <Image source={require('./assets/Home.png')}/>;
+        } else if (routeName === 'ArtWorks') {
+          return <Image source={require('./assets/Art.png')} />;
+        } else if (routeName === 'Galleries') {
+          return <Image source={require('./assets/Gallery.png')} />;
+        } else if (routeName === 'Cart') {
+          return <Image source={require('./assets/Cart.png')} />;
+        } else if (routeName === 'Profile') {
+          return <Image source={require('./assets/Profile.png')} />;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
+
+export default createAppContainer(TabNavigator);
