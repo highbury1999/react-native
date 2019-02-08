119615ecb1b0b06d9ff42aeb287cbeb6f86f4207
henry liao <henryliao@henrys-MBP.lan>
2019-02-08 18:35:46 +1100

    Fix profile UI issue

--- Components/Profile.js
+++ Components/Profile.js
@@ -6,12 +6,27 @@ import Option from './Profile/Option';
 class Profile extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.container}>
         <Header/>
         <Option/>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  subContainer: {
+    flex: 1,
+    width:'100%',
+    backgroundColor: '#636363',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    flex:1,
+    justifyContent:'space-around',
+    alignItems:'center'
+  }
+});
 
 export default Profile;
--- Components/Profile/Option.js
+++ Components/Profile/Option.js
@@ -4,11 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 class Option extends React.Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Profile Option</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth:0.5,
+    borderColor:'#d6d7da',
+    borderRadius:4,
+    padding:20
+  }
+});
 
 export default Option;
