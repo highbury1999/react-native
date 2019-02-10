509f0fda4735cd8af988f8ee697530b89471c831
henry liao <henryliao@henrys-MBP.lan>
2019-02-11 10:46:00 +1100

    Flex UI display

--- Components/Art.js
+++ Components/Art.js
@@ -1,36 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { BarCodeScanner, Permissions } from 'expo';
+import { SafeAreaView } from 'react-navigation';
 
 class Art extends React.Component {
-  state = {
-    hasCameraPermission: null,
-  }
-  async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-    }
-  render() {
-    const { hasCameraPermission } = this.state;
 
-    if (hasCameraPermission === null) {
-      return <Text>Requesting for camera permission</Text>;
-    }
-    if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    }
+
+  render() {
     return (
-      <View style={{ flex: 1 }}>
-        <BarCodeScanner
-          onBarCodeScanned={this.handleBarCodeScanned}
-          style={StyleSheet.absoluteFill}
-        />
-      </View>
+      <SafeAreaView>
+        <View >
+          <Text>This is the art display UI</Text>
+        </View>
+      </SafeAreaView>
     );
   }
-  handleBarCodeScanned = ({ type, data }) => {
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width:'100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Art;
--- Components/Galleries.js
+++ Components/Galleries.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
 
 class Galleries extends React.Component {
   constructor(props){
@@ -7,8 +8,43 @@ class Galleries extends React.Component {
   }
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>This is the galleries section of the project</Text>
+      <View style={[styles.container]}>
+        <View style={[styles.navigation]}>
+          <View style={[styles.naviTitle]}>
+            <Text>ArtWorks</Text>
+          </View>
+        </View>
+        <View style={[styles.tabWrapper]}>
+          <View style={[styles.tabs]}>
+            <View style={[styles.tab]}>
+              <Text>Show All</Text>
+            </View>
+            <View style={[styles.tab]}>
+              <Text>Featured</Text>
+            </View>
+            <View style={[styles.tab]}>
+              <Text>Favourites</Text>
+            </View>
+          </View>
+        </View>
+        <View style={[styles.contentWrapper]}>
+          <View styles={[styles.content]}>
+            <View styles={[styles.block]}>
+              <View style={[styles.img]}>
+                <Text>img</Text>
+              </View>
+              <View style={[styles.title]}>
+                <Text>title</Text>
+              </View>
+              <View style={[styles.description]}>
+                <Text>description</Text>
+              </View>
+              <View style={[styles.price]}>
+                <Text>price</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
@@ -18,10 +54,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width:'100%',
-    backgroundColor: '#636363',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  navigation: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#636363',
+    width:'100%'
+  },
+  tabWrapper:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'orange',
+    width:'100%'
+
+  },
+  contentWrapper:{
+    flex:9,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+    width:'100%'
+  }
 });
 
 export default Galleries;
