f1409c32862bb78f1e189195c66cb5b3a267cce4
henry liao <henryliao@henrys-MBP.lan>
2019-02-11 16:52:20 +1100

    integrate with gradle

--- Components/Galleries/Content.js
+++ Components/Galleries/Content.js
@@ -17,7 +17,10 @@ class Content extends React.Component {
         {key: 'Jillian'},
         {key: 'Jimmy'},
         {key: 'Julie'},
-      ]
+      ],
+      name:{
+        key:'xuyang liao'
+      }
     }
     console.log(this.state);
 
@@ -34,33 +37,21 @@ class Content extends React.Component {
     return (
       <View style={[styles.contentWrapper]}>
         {
-          this.state.currentTab == 1 && <View styles={[styles.content]}>
+          this.state.currentTab == 1 && <View style={[styles.contentWrapper]}>
+            <View><Text>123</Text></View>
+            <View><Text>234</Text></View>
+          </View>
+        }
+        {
+          this.state.currentTab == 2 && <View style={[styles.mainContent]}>
           <FlatList
           data={this.state.data}
           renderItem={({item}) => <Item item={item}/>}
         />
-          </View>
+        </View>
         }
         {
-          this.state.currentTab == 2 && <View styles={[styles.content]}>
-            <View styles={[styles.block]}>
-              <View style={[styles.img]}>
-                <Text>{this.state.currentTab}</Text>
-              </View>
-              <View style={[styles.title]}>
-                <Text>title222</Text>
-              </View>
-              <View style={[styles.description]}>
-                <Text>description222</Text>
-              </View>
-              <View style={[styles.price]}>
-                <Text>price</Text>
-              </View>
-            </View>
-          </View>
-        }
-        {
-          this.state.currentTab == 3 && <View styles={[styles.content]}>
+          this.state.currentTab == 3 && <View styles={[styles.mainContent]}>
             <View styles={[styles.block]}>
               <View style={[styles.img]}>
                 <Text>{this.state.currentTab}</Text>
@@ -88,6 +79,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: 'red',
     width:'100%'
+  },
+  mainContent:{
+    borderWidth:2,
+    backgroundColor:'grey',
+    flexDirection:'row',
+    flex:1
   }
 });
 
--- Components/Galleries/Item.js
+++ Components/Galleries/Item.js
@@ -21,9 +21,6 @@ class Item extends React.Component {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-    flexDirection:'row'
   }
 })
 export default Item;
